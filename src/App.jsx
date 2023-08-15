abef1bd10dd1eedb1f292f14dc8eab3ba43d527d
codeark1 <ajitenadavid@gmail.com>
2023-08-15 16:56:23 +0100

    signup form complete

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Registration from './Component/Registration'
+
+
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Registration/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component/Login.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className=' grid grid-cols-2  h-screen'>
+<div className='border bg-red-500 grid place-content-center'>
+  <div className=' border'>fffer</div>
+  <div>ergtegtrg</div>
+
+
+
+
+</div>
+<div></div>
+      
+
+
+    </div>
+  )
+}
+
+export default Login
