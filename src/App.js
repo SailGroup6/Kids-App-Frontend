9eb1ac4595aee0fca91fa753003750a65ddc3522
codeark1 <128057722+Codeark1@users.noreply.github.com>
2023-08-19 15:37:09 +0100

    update on the profile page and title for the widgets
    update on the profile page and widget routing and widget title

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import LandingPage from "./Pages/LandingPage/LandingPage";
 import Dashboard from "./Pages/DashboardPage/Dashboard";
 import LoginPage from "./Pages/LoginPage/LoginPage";
 import Registration from "./Pages/RegistrationPage/Registration";
-import AboutUs from "./Pages/LandingPage/AboutUs/AboutUs";
+import Profile from "./Pages/DashboardPage/Profile";
+import { ConfigProvider } from "antd";
+
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/registration" element={<Registration />} />
-      </Routes>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#9B5DE6",
+          },
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/Profile" element={<Profile/>}/>
+        </Routes>
+      </ConfigProvider>
+    
     </div>
   );
 }
--- src/Components/Cards/WidgetCards.jsx
+++ src/Components/Cards/WidgetCards.jsx
@@ -9,23 +9,26 @@ import logoutIcon from "../../Assets/Images/log-out.svg";
 
 const WidgetCards = () => {
   const images = [
-    { name: dashboardIcon, style: "max-w-[80%] mb-[1.5rem] mt-[5rem]", link:"/dashboard" },
-    { name: smileyIcon, style: "max-w-[80%] mb-[1.5rem]", link:"/smiley" },
-    { name: trendingIcon, style: "max-w-[80%] mb-[1.5rem]", link:"/trending" },
-    { name: clipboardIcon, style: "max-w-[80%] mb-[15rem]", link:"/clipboard" },
-    { name: settingsIcon, style: "max-w-[80%] mb-[1.5rem]", link:"/settings" },
-    { name: logoutIcon, style: "max-w-[80%] mb-[1.5rem]", link:"/logout" }
+    { name: dashboardIcon, style: "max-w-[80%] mb-[1.5rem] mt-[5rem]", link:"/dashboard", title:"Dashboard" },
+    { name: smileyIcon, style: "max-w-[80%] mb-[1.5rem]", link:"/Profile", title:"Profile" },
+    { name: trendingIcon, style: "max-w-[80%] mb-[1.5rem]", link:"/trending", title:"Progress" },
+    { name: clipboardIcon, style: "max-w-[80%] mb-[15rem]", link:"/clipboard", title: "Clipboard" },
+    { name: settingsIcon, style: "max-w-[80%] mb-[1.5rem]", link:"/settings", title: "setting" },
+    { name: logoutIcon, style: "max-w-[80%] mb-[1.5rem]", link:"/logout", title: "Log out" }
   ];
   return (
     <div>
       {images.map((image, index) => (
         <div className="">
-          <NavLink to={image.link}>
+          <NavLink to={image.link} >
             <img
               key={index}
               src={image.name}
               alt="Icon"
               className={image.style}
+              title={image.title}
+    
+             
             />
           </NavLink>
         </div>
