1aa7914938b1168d311a437cb10c22361707ce08
Abiola Sodiq <rasababe01@gmail.com>
2023-08-22 00:40:54 +0100

    updated

--- src/Pages/LandingPage/HeroSection/Arrow.jsx
+++ src/Pages/LandingPage/HeroSection/Arrow.jsx
@@ -1,12 +1,14 @@
+// Importing required dependencies and components
 import React from "react";
 import { Link } from "react-scroll";
-
 import { motion } from "framer-motion";
 import ArrowImage from "../../../Assets/Images/arrow.svg";
 
+// Define a functional component called Arrow
 const Arrow = () => {
   return (
     <div>
+    {/* Adding framer-motion animation to the div */}
       <motion.div
         className="rounded-full cursor-pointer"
         href="#aboutUsId"
@@ -16,12 +18,14 @@ const Arrow = () => {
           boxShadow: "0px 0px 8px rgb(0,0,255)",
         }}
       >
+      {/* Creating a smooth scroll link using react-scroll */}
         <Link to="aboutUsLink" smooth={true} duration={1500}>
-          <img src={ArrowImage} />
+          <img src={ArrowImage} alt="Arrow" />
         </Link>
       </motion.div>
     </div>
   );
 };
 
-export default Arrow;
+// Exporting the Arrow component to be used in other files
+export default Arrow; 
--- src/Pages/LandingPage/HeroSection/HeroSectionText.jsx
+++ src/Pages/LandingPage/HeroSection/HeroSectionText.jsx
@@ -1,10 +1,13 @@
+// Importing the necessary module
 import React from "react";
 
+// Defining a functional component called HeroSectionText
 const HeroSectionText = () => {
   return (
     <div className="flex flex-col text-center text-4xl md:text-6xl font-[caveat-regular]">
       <h1>
-        Empowering Young Minds, <br />
+        Empowering Young Minds, <br /> 
+        {/* Adding a gradient-colored span for a stylish text effect */}
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#1DA684] to-[#BEDC7CFC]">
           One Click at a Time!
         </span>
@@ -13,4 +16,6 @@ const HeroSectionText = () => {
   );
 };
 
-export default HeroSectionText;
+// Exporting the HeroSectionText component for use in other files
+
+export default HeroSectionText; 
