1fe140e40c5946caa0c28aecfd2c6d8ad365024c
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-08-21 19:40:23 +0100

    Update NotFoundPage.jsx

--- src/Pages/NotFoundPage/NotFoundPage.jsx
+++ src/Pages/NotFoundPage/NotFoundPage.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
-const NotFoundPage = () =>{
-    const navigate=useNavigate();
-    return(
-        <React.Fragment>
-            <div>
-                
-
-            </div>
-
-        </React.Fragment>
-    )
-}
+const NotFoundPage = () => {
+  const navigate = useNavigate();
+  return (
+    <React.Fragment>
+      <motion.div>
+   (
+        <Result
+          status="404"
+          title="404"
+          subTitle="Sorry, the page you visited does not exist."
+          extra={<Button type="primary">Back Home</Button>}
+        />
+        ); 
+      </motion.div>
+    </React.Fragment>
+  );
+};
+export default NotFoundPage;
