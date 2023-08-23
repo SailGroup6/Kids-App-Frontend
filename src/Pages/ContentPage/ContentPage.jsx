71bd52a65601b105f7bc07c0abf49c60a7bf1a52
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-08-23 09:43:05 +0100

    Update ContentPage.jsx

--- src/Pages/ContentPage/ContentPage.jsx
+++ src/Pages/ContentPage/ContentPage.jsx
@@ -1,28 +1,14 @@
 import React from "react";
 import WidgetCards from "../../Components/Cards/WidgetCards";
 import { useState, useEffect } from "react";
-import { useLocation, useNavigate, Link } from "react-router-dom";
-import VideoPlayer from "../../Components/Video/VideoPlayer";
+import { useNavigate, Link } from "react-router-dom";
 import { Layout, Spin } from "antd";
 import BackIcon from "../../Assets/Images/bx-arrow-back.svg";
 import { motion } from "framer-motion";
-import { Divider, Menu, Switch } from 'antd';
-import {
-  AppstoreOutlined,
-  CalendarOutlined,
-  LinkOutlined,
-  MailOutlined,
-  SettingOutlined,
-} from '@ant-design/icons';
-import WidgetCardsSmiley from "../../Components/Cards/WidgetCardsSmiley";
+import { Menu } from "antd";
+import { AppstoreOutlined } from "@ant-design/icons";
 
-const { Header, Content, Sider } = Layout;
-const storedUsername = localStorage.getItem("username") || "User";
-const storedGender = localStorage.getItem("gender") || "";
-const storedAge = localStorage.getItem("age") || "";
-const storedFullName = localStorage.getItem("fullName") || "";
-const storedEmail = localStorage.getItem("email") || "";
-const storedParentFullName = localStorage.getItem("parentFullName") || "";
+const { Content, Sider } = Layout;
 
 //const videos = [{ src: "" }];
 
@@ -35,28 +21,40 @@ function getItem(label, key, icon, children) {
   };
 }
 const items = [
-
-  getItem('HTML BASICS', 'sub1', <AppstoreOutlined />, [
-    getItem('Lesson 1: Introduction to basic HTML', '1'),
-    getItem('Lesson 2: HTML page structure', '2'),
-    getItem('Lesson 3: H1 element', '3'),
-    getItem('Lesson 4: Paragraph', '4'),
-    getItem('Lesson 5: Image tag ', '5'),
-    getItem('Lesson 6: Image height and width', '6'),
-    getItem('Lesson 7: Link Tag', '7'),
-    getItem('Lesson 8: Linking Images', '8'),
-    getItem('Lesson 9: Div container', '8'),
-    
-
-
-  ])
+  getItem("HTML BASICS", "sub1", <AppstoreOutlined />, [
+    getItem("Lesson 1: Introduction to basic HTML", "1"),
+    getItem("Lesson 2: HTML page structure", "2"),
+    getItem("Lesson 3: H1 element", "3"),
+    getItem("Lesson 4: Paragraph", "4"),
+    getItem("Lesson 5: Image tag ", "5"),
+    getItem("Lesson 6: Image height and width", "6"),
+    getItem("Lesson 7: Link Tag", "7"),
+    getItem("Lesson 8: Linking Images", "8"),
+    getItem("Lesson 9: Div container", "9"),
+  ]),
 ];
-const Profile = () => {
+
+const videoSources = [
+  { name: "lesson1", path: "https://m.youtube.com/watch?v=BpgAzeU0w-s" },
+  { name: "lesson2", path: "https://m.youtube.com/watch?v=MskilKYVKOs" },
+  { name: "lesson3", path: "https://m.youtube.com/watch?v=P6CUDf-bu5A" },
+  { name: "lesson4", path: "https://m.youtube.com/watch?v=LIj5w7gAmXc" },
+  { name: "lesson5", path: "https://m.youtube.com/watch?v=2KGR6tQ5B0U" },
+  { name: "lesson6", path: "https://m.youtube.com/watch?v=LmoMDpEncEE" },
+  { name: "lesson7", path: "https://m.youtube.com/watch?v=vq_QwWhlHhc" },
+  { name: "lesson8", path: "https://m.youtube.com/watch?v=ed7iFE7VavI" },
+];
+const ContentPage = () => {
   const [mode, setMode] = useState("inline");
+  const [selectedVideoIndex, setSelectedVideoIndex] = useState(0);
+  const [isLoading, setIsLoading] = useState(true); // Initialize with the first video link
   const changeMode = (value) => {
     setMode(value ? "vertical" : "inline");
   };
-  const [isLoading, setIsLoading] = useState(true);
+
+  const changeVideo = (index) => {
+    setSelectedVideoIndex(index);
+  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -79,10 +77,21 @@ const Profile = () => {
             whileHover={{ scale: 1.3, transition: { duration: 0.1 } }}
             className="p-[1rem]"
           />
-        </Link> {/*bg-[rgba(155, 93, 230, 0.30)]*/}
+        </Link>{" "}
+        {/*bg-[rgba(155, 93, 230, 0.30)]*/}
         <Content className="h-full grid p-10 ">
           <motion.div className="w-full">
-            <VideoPlayer />
+            <iframe
+              width="100%"
+              height="50%"
+              src={videoSources[selectedVideoIndex].src}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              className="w-[100%] h-full object-cover rounded-xl"
+              allowFullScreen
+            ></iframe>
+
             <p className="font-[poppins-regular] p-[1rem]">
               Uploaded by <span className="font-bold">Godslove Udo</span>
             </p>
@@ -99,7 +108,6 @@ const Profile = () => {
             backgroundColor: "#fafafa",
             height: "100svh",
           }}
-
         >
           <Menu
             defaultSelectedKeys={["1"]}
@@ -107,11 +115,24 @@ const Profile = () => {
             mode={mode}
             items={items}
             className="font-[poppins-regular] "
-          ></Menu>
+            onClick={(e) => {
+              const selectedIndex = videoSources.findIndex(
+                (video) => video.name === e.key
+              );
+              if (selectedIndex !== -1) {
+                changeVideo(selectedIndex);
+              }
+            }}
+          >
+            {" "}
+            {videoSources.map((video) => (
+              <Menu.Item key={video.name}>{video.name}</Menu.Item>
+            ))}
+          </Menu>
         </Sider>
       </Layout>
     </div>
   );
 };
 
-export default Profile;
+export default ContentPage;
